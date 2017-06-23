7b8b0db9a0d2e7ed5a7165eb90cea9ff291cc04d
Yang Shi <shiyanghp@hotmail.com>
2017-06-23 03:10:39 -0700

    AddPolyD2Regression

---
+++ Poly2RegresionTest/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Poly2RegresionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.MenuFileOpen = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.dgvLearningSource = new System.Windows.Forms.DataGridView();
+            this.dgvTestingSource = new System.Windows.Forms.DataGridView();
+            this.Learning = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLearningSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTestingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(561, 356);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // MenuFileOpen
+            // 
+            this.MenuFileOpen.Location = new System.Drawing.Point(463, 393);
+            this.MenuFileOpen.Name = "MenuFileOpen";
+            this.MenuFileOpen.Size = new System.Drawing.Size(75, 23);
+            this.MenuFileOpen.TabIndex = 1;
+            this.MenuFileOpen.Text = "ReadDataSet";
+            this.MenuFileOpen.UseVisualStyleBackColor = true;
+            this.MenuFileOpen.Click += new System.EventHandler(this.MenuFileOpen_Click_1);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // dgvLearningSource
+            // 
+            this.dgvLearningSource.AllowUserToOrderColumns = true;
+            this.dgvLearningSource.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLearningSource.Location = new System.Drawing.Point(579, 12);
+            this.dgvLearningSource.Name = "dgvLearningSource";
+            this.dgvLearningSource.RowTemplate.Height = 23;
+            this.dgvLearningSource.Size = new System.Drawing.Size(145, 356);
+            this.dgvLearningSource.TabIndex = 2;
+            // 
+            // dgvTestingSource
+            // 
+            this.dgvTestingSource.AllowUserToOrderColumns = true;
+            this.dgvTestingSource.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTestingSource.Location = new System.Drawing.Point(744, 12);
+            this.dgvTestingSource.Name = "dgvTestingSource";
+            this.dgvTestingSource.RowTemplate.Height = 23;
+            this.dgvTestingSource.Size = new System.Drawing.Size(145, 356);
+            this.dgvTestingSource.TabIndex = 3;
+            // 
+            // Learning
+            // 
+            this.Learning.Location = new System.Drawing.Point(579, 396);
+            this.Learning.Name = "Learning";
+            this.Learning.Size = new System.Drawing.Size(75, 23);
+            this.Learning.TabIndex = 4;
+            this.Learning.Text = "Learning";
+            this.Learning.UseVisualStyleBackColor = true;
+            this.Learning.Click += new System.EventHandler(this.Learning_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 431);
+            this.Controls.Add(this.Learning);
+            this.Controls.Add(this.dgvTestingSource);
+            this.Controls.Add(this.dgvLearningSource);
+            this.Controls.Add(this.MenuFileOpen);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLearningSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTestingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button MenuFileOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dgvLearningSource;
+        private System.Windows.Forms.DataGridView dgvTestingSource;
+        private System.Windows.Forms.Button Learning;
+    }
+}
+
---
+++ Poly2RegresionTest/Form1.cs
@@ -0,0 +1,145 @@
+﻿using Accord.IO;
+using Accord.Math;
+using Accord.Math.Optimization.Losses;
+using Accord.Statistics.Models.Regression.Linear;
+using Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Poly2RegresionTest
+{
+    public partial class Form1 : Form
+    {
+        string[] columnNames;
+        public Form1()
+        {
+            InitializeComponent();
+            columnNames = new string[2];
+            columnNames[0] = "x";
+            columnNames[1] = "y";
+        }
+
+
+        public void PolyRegression()
+        {
+
+            if (dgvTestingSource.DataSource == null)
+            {
+                MessageBox.Show("Please Select a data set");
+                return;
+            }
+
+
+            // Creates a matrix from the source data table
+            double[,] table = (dgvLearningSource.DataSource as DataTable).ToMatrix();
+
+            double[,] data = table;
+
+            // Let's retrieve the input and output data:
+            double[] inputs = data.GetColumn(0);  // X
+            double[] outputs = data.GetColumn(1); // Y
+
+            // We can create a learning algorithm
+            var ls = new PolynomialLeastSquares()
+            {
+                Degree = 2
+            };
+
+            // Now, we can use the algorithm to learn a polynomial
+            PolynomialRegression poly = ls.Learn(inputs, outputs);
+
+            // The learned polynomial will be given by
+            string str = poly.ToString("N1"); // "y(x) = 1.0x^2 + 0.0x^1 + 0.0"
+
+            // Where its weights can be accessed using
+            double[] weights = poly.Weights;   // { 1.0000000000000024, -1.2407665029287351E-13 }
+            double intercept = poly.Intercept; // 1.5652369518855253E-12
+
+            // Finally, we can use this polynomial
+            // to predict values for the input data
+            double[] pred = poly.Transform(inputs);
+
+            // Where the mean-squared-error (MSE) should be
+            double error = new SquareLoss(outputs).Loss(pred); // 0.0
+
+            double[][] tmpInputs = new double[inputs.Length][];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                tmpInputs[i] = new double[1]{ inputs[i] };
+            }
+            CreateResultScatterplot(zedGraphControl1, tmpInputs, outputs, pred);
+        }
+        public void CreateResultScatterplot(ZedGraphControl zgc, double[][] inputs, double[] expected, double[] output)
+        {
+            GraphPane myPane = zgc.GraphPane;
+            myPane.CurveList.Clear();
+
+            // Set the titles
+            myPane.Title.IsVisible = false;
+            myPane.XAxis.Title.Text = columnNames[0];
+            myPane.YAxis.Title.Text = columnNames[1];
+
+
+            // Regression problem
+            PointPairList list1 = new PointPairList(); // svm output
+            PointPairList list2 = new PointPairList(); // expected output
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                list1.Add(inputs[i][0], output[i]);
+                list2.Add(inputs[i][0], expected[i]);
+            }
+
+            // Add the curve
+            LineItem myCurve = myPane.AddCurve("Model output", list1, Color.Blue, SymbolType.Diamond);
+            myCurve.Line.IsVisible = true;
+            myCurve.Symbol.Border.IsVisible = false;
+            myCurve.Symbol.Fill = new Fill(Color.Blue);
+
+            myCurve = myPane.AddCurve("Data output", list2, Color.Red, SymbolType.Diamond);
+            myCurve.Line.IsVisible = false;
+            myCurve.Symbol.Border.IsVisible = false;
+            myCurve.Symbol.Fill = new Fill(Color.Red);
+
+            // Fill the background of the chart rect and pane
+            myPane.Fill = new Fill(Color.WhiteSmoke);
+
+            zgc.AxisChange();
+            zgc.Invalidate();
+        }
+
+        private void MenuFileOpen_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string filename = openFileDialog.FileName;
+                string extension = Path.GetExtension(filename);
+                if (extension == ".xls" || extension == ".xlsx")
+                {
+                    ExcelReader db = new ExcelReader(filename, true, false);
+                    TableSelectDialog t = new TableSelectDialog(db.GetWorksheetList());
+
+                    if (t.ShowDialog(this) == DialogResult.OK)
+                    {
+                        DataTable tableSource = db.GetWorksheet(t.Selection);
+                        this.dgvLearningSource.DataSource = tableSource;
+                        this.dgvTestingSource.DataSource = tableSource.Copy();
+                    }
+                }
+            }
+        }
+
+        private void Learning_Click(object sender, EventArgs e)
+        {
+            PolyRegression();
+        }
+    }
+}
